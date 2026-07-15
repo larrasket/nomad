6cc5836f7bf425f6e254fb26bbcde496ad39dee9
Allison Larson <allisonlarson91@gmail.com>
2026-07-15 08:39:46 -0700

    add fifo queue implementation

--- nomad/queues/queues.go
+++ nomad/queues/queues.go
@@ -6,6 +6,7 @@ package queues
 import (
 	"github.com/hashicorp/go-hclog"
 	dynamic "github.com/hashicorp/nomad/nomad/queues/dynamic_priority"
+	"github.com/hashicorp/nomad/nomad/queues/fifo"
 	"github.com/hashicorp/nomad/nomad/queues/passthrough"
 	"github.com/hashicorp/nomad/nomad/queues/queue"
 	"github.com/hashicorp/nomad/nomad/structs"
@@ -19,6 +20,8 @@ func NewQueue(sconf *structs.BatchQueue, broker queue.Broker, logger hclog.Logge
 			return nil, err
 		}
 		return dynamic.NewDynamicPriorityQueue(broker, sconf, qconf, logger), nil
+	case structs.BatchQueueTypeFifo:
+		return fifo.NewFifoQueue(broker, logger), nil
 	default:
 		return passthrough.NewPassthroughQueue(broker), nil
 	}
